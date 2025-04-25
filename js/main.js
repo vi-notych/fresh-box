4e02929acdf169deaf69c45601e033c5633aed18
vi-notych <vin.arnet@gmail.com>
2025-04-25 12:33:18 +0300

    сделал header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 import { useDynamicAdapt } from "./dynamicAdapt.js";
 
 useDynamicAdapt();
+
+function init() {
+  mobileMenu();
+}
+document.addEventListener("DOMContentLoaded", init);
+
+/**
+ * BurgerMenu
+ */
+function mobileMenu() {
+  const btn = document.querySelector(".header__button");
+  const mobileMenu = document.querySelector(".header__menu");
+  const body = document.querySelector("body");
+  btn.addEventListener("click", () => {
+    mobileMenu.classList.toggle('active');
+    body.classList.toggle('no-scroll');
+    btn.classList.toggle('active');
+  });
+}
